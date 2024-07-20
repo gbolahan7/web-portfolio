f4aa68765977cb7c4b6b5af6c65e98c8a1315301
Abass Durodola <abass_gbolahan@yahoo.com>
2024-07-20 13:51:20 +0100

    new update

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    ScrollReveal().reveal('#about', {
+        origin: 'left',
+        distance: '50px',
+        duration: 1000,
+        delay: 300
+    });
+
+    ScrollReveal().reveal('#experience', {
+        origin: 'right',
+        distance: '50px',
+        duration: 1000,
+        delay: 300
+    });
+
+    ScrollReveal().reveal('#projects', {
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        delay: 300
+    });
+});
